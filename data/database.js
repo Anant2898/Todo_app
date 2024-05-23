eedd5a5be49fe7231a0a2d6b5d66fe0b71cdf197
Anant2898 <49041548+Anant2898@users.noreply.github.com>
2024-05-23 06:33:03 +0530

    Update database.js

--- data/database.js
+++ data/database.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 export async function connectToDatabase() {
     try {
-      await mongoose.connect(process.env.MONGO_URI , { dbName: "backendapi" });
-      console.log("Backend database connected");
+      const c= await mongoose.connect(process.env.MONGO_URI , { dbName: "backendapi" });
+      console.log(`Backend database connected ${c.connection.host}`);
     } catch (error) {
-      console.error(error);
+      console.error(error); 
     } 
   }
