7dd6a909e38bc006e781a587e6765b52f88544eb
Anant2898 <49041548+Anant2898@users.noreply.github.com>
2024-05-23 06:34:57 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,14 @@
 import express from "express"
-import { getAllUsers, register, login, getMyProfile, logout} from "../controllers/user.js";
+import {  register, login, getMyProfile, logout} from "../controllers/user.js";
 import { isAuth } from "../middlewares/auth.js";
+
 const router = express.Router();
 
-router.get("/all", getAllUsers);
+
 
 router.post("/new", register);
 router.post("/login", login);
 router.get("/logout", logout);
 router.get("/me", isAuth ,getMyProfile);
-
+    
 export default router;
