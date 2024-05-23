cae6784bdfb0af1fb6fb058e8dd7b6dfc23eb255
Anant2898 <49041548+Anant2898@users.noreply.github.com>
2024-05-23 06:36:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ import { connectToDatabase } from "./data/database.js";
 connectToDatabase();
 
 server.listen(process.env.PORT, ()=>{
-    console.log("server's up and running :)");
+    console.log(`server's up and running :) in ${process.env.NODE_ENV } mode ` );
 }); 
